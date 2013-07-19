3520b9b5808187776fa8068d0bd64a1f685a128e
Desmond Bowe <desmondbowe@gmail.com>
2013-07-19 17:07:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BunnyTest::Application.config.secret_key_base = '493a433301840678c4a2bc245f06d422c193bf910d2ddbad9a6c5b37a84fbca40765009c99f73d4ee300e474010017692fe9ab015fa18ba1df87befbd8d7c675'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BunnyTest::Application.config.session_store :cookie_store, key: '_bunny_test_session'
